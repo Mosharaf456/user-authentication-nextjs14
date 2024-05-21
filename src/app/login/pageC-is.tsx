cedb099cda27243863df5dd7b2c884bee46700de
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-21 13:07:45 +0600

    ref login button client side hook not working:updated as is:added DONE

--- src/app/login/pageC-is.tsx
+++ src/app/login/pageC-is.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from 'react';
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 
 import { loginUserAction } from "@/actions/login";
 
@@ -24,29 +24,49 @@ function LoginPageComponent() {
     const [error, setError] = useState("");
 
 
-    const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault(); // Prevent default form submission behavior
         // const formData = new FormData();
         // formData.append("email", signInFormData.username);
         // formData.append("password", signInFormData.password);
         // console.log("formData:::", formData);
 
-        // const result = await loginUserAction(signInFormData);
-        // console.log(result);
-        // if (result?.status) {
-        //     router.push("/dashboard");
-        // } else {
-        //     setError(result?.error || "");
-        // }
+        const result = await loginUserAction(signInFormData);
+        console.log(result);
+        if (result?.status) {
+            router.push("/dashboard");
+        } else {
+            setError(result?.error || "");
+        }
     }
     return (
         <div>
           <h1>Login UI</h1>
             {error && <p>{error}</p>}
 
-          <form onSubmit={handleSignIn}>
-            <button type="submit">UserLogin</button>
-          </form>
+            <form onSubmit={handleSignIn}>
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={signInFormData.username}
+                        onChange={(e) => setSignInFormData({ ...signInFormData, username: e.target.value })}
+                    />
+                </div>
+                <br />
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={signInFormData.password}
+                        onChange={(e) => setSignInFormData({ ...signInFormData, password: e.target.value })}
+                    />
+                </div>
+                <br />
+                <button type="submit">User Login</button>
+            </form>
         </div>
     );
 
