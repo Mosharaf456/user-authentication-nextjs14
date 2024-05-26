3f8d7c742d54d378a59fc1cccdc84b5a916356da
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-26 17:12:26 +0600

    ref middleware working as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import LoginComponent from "@/components/custom-ui/LoginComponent/LoginComponent";
-import Login from "@/app/login/page";
 
 export default function Home() {
   return (
-      <Login />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <LoginComponent />
+    </main>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,30 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { updateSession, getSession } from "@/app/actions/lib";
 
 
 export async function middleware(request: NextRequest) {
     await updateSession(request);
+    console.log('session updated');
     const session = await getSession();
-    console.log('session ', session);
 
     const path = request.nextUrl.pathname;
-    const checkPath = path === '/login' || path === '/signup' || path ==='/';
-    console.log('path ', path);
 
-    try {
-        if(checkPath && session !== null) {  // user will not go to the login page if already logged in
-            return NextResponse.redirect(new URL('/dashboard' , request.url));
-        }
-        if(!checkPath && session === null) { //any path that is not login or signup will be redirected to login page if not logged in
-            return NextResponse.redirect(new URL('/login' , request.url));
-        }
-    }catch(e) {
-        console.log('middleware error ***********', e);
+    const checkPublicPath = path === "/login" || path === "/" ;
+    console.log('path is=======26MAY====== ', checkPublicPath);
+
+    if(checkPublicPath && session !== null) {  // user will not go to the login page if already logged in
+        console.log('>>>>>>redirecting to dashboard>>>>>>>>');
+        return NextResponse.redirect(new URL('/dashboard' , request.nextUrl));
     }
-
+    if(!checkPublicPath && session === null) { //any path that is not login or signup will be redirected to login page if not logged in
+        return NextResponse.redirect(new URL('/login' , request.nextUrl));
+    }
 
 }
 
 export const config = {
-    matcher: ['/about/:path*', '/dashboard/:path*', '/login'],
+    matcher: ["/about/:path*", "/dashboard/:path*", "/login","/"],
 };
+
+// ref: https://github.com/vercel/next.js/blob/canary/examples/middleware/middleware.ts
+
