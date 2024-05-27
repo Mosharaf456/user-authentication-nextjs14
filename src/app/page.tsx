02331e716d964e6e7d44530c494df76c11745ce2
mosharaf456 <mosharaf111hossain@gmail.com>
2024-05-27 14:46:08 +0600

    ref mui theme and mui installed into NEXT JS v14:updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 
 import BackgroundTest from "@/components/background";
-import { Typography } from "@mui/material";
+import { Box, Typography } from '@mui/material';
 
 export default function Home() {
   return (
     <>
-      <Typography variant="h3" sx={{ml: 10, textAlign: "center"}}> NEXT JS (Material UI)MUI App v5 Installation and Theme customize </Typography>
+      <Box sx={{ p: 3, bgcolor: 'background.default', color: 'text.primary' }}>
+        <Typography variant="h3" sx={{ml: 10, textAlign: "center"}}> NEXT JS (Material UI)MUI App v5 Installation and Theme customize </Typography>
+      </Box>
       <BackgroundTest />
     </>
   );
